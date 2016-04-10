1c91d09427a9b14e4bbe1b82e5f352962eff2893
Everton Muniz <munizeverton@gmail.com>
2016-04-10 18:11:11 +0000

    Criando front end

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,20 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['middleware' => ['web']], function () {
+    Route::get('/', 'HomeController@index');
+    Route::get('/imovel/{id}', 'HomeController@viewImovel');
+
+
+    // Rotas da autenticacao
+    Route::get('/login', 'Auth\AuthController@getLogin');
+    Route::post('/auth', 'Auth\AuthController@postLogin');
+    Route::get('/logout', 'LoginController@logout');
 });
+
+Route::group(['middleware' => ['web', 'auth']], function () {
+    Route::get('/admin', 'AdminController@index');
+    Route::get('/admin/imovel/novo', 'AdminController@createForm');
+    Route::get('/admin/imovel/{id}', 'AdminController@getImovel');
+});
+
---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,45 @@
+@extends('template')
+
+@section('content')
+    <div class="col-md-12">
+        <table class="table table-striped table-hover" id="datatable">
+            <thead>
+            <tr>
+                <th>
+                    Endereço
+                </th>
+                <th>
+                    Valor do aluguel
+                </th>
+                <th>
+                    Detalhes
+                </th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach($imoveis as $imovel)
+                <tr>
+                    <td>
+                        {{$imovel->endereco}}, {{$imovel->cidade}} - {{$imovel->estado}}
+                    </td>
+                    <td>
+                        {{number_format($imovel->valor_aluguel, 2, ',', '.')}}
+                    </td>
+                    <td>
+                        <a href="/imovel/{{$imovel->id}}"><span class="glyphicon glyphicon-plus-sign"
+                                                                     aria-hidden="true"></span></a>
+                    </td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    </div>
+@endsection
+
+@section('scripts')
+    <script>
+        $(document).ready(function() {
+            $('#datatable').DataTable();
+        } );
+    </script>
+@endsection
